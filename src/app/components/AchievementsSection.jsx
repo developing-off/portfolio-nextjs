ad9198af019003aa12a620a68fac66ce995a7e53
Younes Sarni <ysarni42@gmail.com>
2023-11-11 14:24:27 +0100

    number change

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -12,21 +12,21 @@ const AnimatedNumbers = dynamic(
 const achievementsList = [
   {
     metric: "Projects",
-    value: 100,
+    value: 8,
     postfix: "+",
   },
   {
     prefix: "~",
     metric: "Users",
-    value: 100,
+    value: 2,
   },
   {
     metric: "Awards",
-    value: 7,
+    value: 2,
   },
   {
     metric: "Years",
-    value: "5",
+    value: 1,
   },
 ];
 
